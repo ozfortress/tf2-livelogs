fe7aabd67d35656184bd37335f1b3d49eb1da47f
Prithu Parker <prithu@iinet.net.au>
2012-10-09 19:06:36 +1100

    all regex in, building pgsql functions and tables

--- parser.py
+++ parser.py
@@ -11,21 +11,21 @@ class parserClass():
             self.bNamedLog = True
             self.namedLogName = ipgnBooker
 
-
+        
         self.serverSendingLogs = server_address
 
         self.UNIQUE_IDENT = str(self.ip2long(server_address[0])) + "_" + str(server_address[1]) + "_" + str(int(round(time.time())))
 
         print "PARSER UNIQUE IDENT: " + self.UNIQUE_IDENT
-
+        
 
         try:
             self.psqlConn = psycopg2.connect(host="localhost", port="5432", database="livelogs", user="livelogs", password="hello")
-
+    
         except Exception, e:
             print "Had exception while trying to connect to psql database: " + e.pgerror
 
-
+        
         self.dbCursor = self.psqlConn.cursor()
 
         query = "CREATE TABLE log_%s (id serial PRIMARY KEY, num int, text varchar)" % self.UNIQUE_IDENT
@@ -75,38 +75,272 @@ class parserClass():
             print "Log file started"
             pprint(res.groups())
             ##do shit with log file name?
+            
+            return
+
+        #log time
+        res = regex(r'L (\S+) - (\S+)', logdata)
+        if (res):
+            print "Time of current log"
+            pprint(res.groups())
+            
+            return
+
+        #damage dealt
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "damage" \x28damage "(\d+)"\x29', logdata)
+        if (res):
+            print "Damage dealt"
+            pprint(res.groups())
+
+            return
+
+        #healing done
+        #"vsn.RynoCerus<6><STEAM_0:0:23192637><Blue>" triggered "healed" against "Hyperbrole<3><STEAM_0:1:22674758><Blue>" (healing "26")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "healed" against "(.*)<(\d+)><(.*)><(Red|Blue)>" \x28healing "(\d+)"\x29', logdata)
+        if (res):
+            print "Healing done"
+            pprint(res.groups())
+
+            return
+
+        #item picked up
+        #"skae<14><STEAM_0:1:31647857><Red>" picked up item "ammopack_medium"
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" picked up item "(.*)"', logdata)
+        if (res):
+            print "Item picked up"
+            pprint(res.groups())
+        
+            return
+
+        #player killed (normal)
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" killed "(.*)<(\d+)><(.*)><(Red|Blue)>" with "(.*)" \x28attacker_position "(.*)"\x29 \x28victim_position "(.*)"\x29', logdata)
+        if (res):
+            print "Player killed (normal kill)"
+            pprint(res.groups())
+
+            return
+
+        #player killed (special kill) 
+        #"Liquid'Time<41><STEAM_0:1:19238234><Blue>" killed "[v3] Roight<53><STEAM_0:0:8283620><Red>" with "knife" (customkill "backstab") (attacker_position "-1085 99 240") (victim_position "-1113 51 240")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" killed "(.*)<(\d+)><(.*)><(Red|Blue)>" with "(.*)" \x28customkill "(.*)"\x29 \x28attacker_position "(.*)"\x29 \x28victim_position "(.*)"\x29', logdata)
+        if (res):
+            print "Player killed (customkill)"
+            pprint(res.groups())
+
+            return
+        
+        #player assist
+        #"Iyvn<40><STEAM_0:1:41931908><Blue>" triggered "kill assist" against "[v3] Kaki<51><STEAM_0:1:35387674><Red>" (assister_position "-905 -705 187") (attacker_position "-1246 -478 237") (victim_position "-1221 -53 283")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "kill assist" against "(.*)<(\d+)><(.*)><(Red|Blue)>" \x28assister_position "(.*)"\x29 \x28attacker_position "(.*)"\x29 \x28victim_position "(.*)"\x29', logdata)
+        if (res):
+            print "Player assisted in kill"
+            pprint(res.groups())
+
+            return
+
+        #medic death ubercharge = 0 or 1, healing = amount healed in that life. kill message comes directly after
+        #"%s<%i><%s><%s>" triggered "medic_death" against "%s<%i><%s><%s>" (healing "%d") (ubercharge "%s")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "medic_death" against "(.*)<(\d+)><(.*)><(Red|Blue)>" \x28healing "(.*)"\x29 \x28ubercharge "(.*)"\x29)', logdata)
+        if (res):
+            print "Medic death"
+            pprint(res.groups())
+
+            return
+
+        #ubercharge used
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "chargedeployed"', logdata)
+        if (res):
+            print "Ubercharge used"
+            pprint(res.groups())
+            
+            return
+
+        #point capture
+        #/Team "(Blue|Red)" triggered "pointcaptured" \x28cp "(\d+)"\x29 \x28cpname "(.+)"\x29 \x28numcappers "(\d+)".+/
+        #Team "Red" triggered "pointcaptured" (cp "0") (cpname "#koth_viaduct_cap") (numcappers "5") (player1 "[v3] Faithless<47><STEAM_0:0:52150090><Red>") (position1 "-1370 59 229") (player2 "[v3] Chrome<48><STEAM_0:1:41365809><Red>") (position2 "-1539 87 231") (player3 "[v3] Jak<49><STEAM_0:0:18518582><Red>") (position3 "-1659 150 224") (player4 "[v3] Kaki<51><STEAM_0:1:35387674><Red>") (position4 "-1685 146 224") (player5 "[v3] taintedromance<52><STEAM_0:0:41933053><Red>") (position5 "-1418 182 236")
+        res = regex(r'Team "(Blue|Red)" triggered "pointcaptured" \x28cp "(\d+)"\x29 \x28cpname "(.*)"\x29 \x28numcappers "(\d+)"', logdata)
+        if (res):
+            print "Point captured"
+            pprint(res.groups())
+
+            return
+
+        #capture block
+        #"pvtx<103><STEAM_0:1:7540588><Red>" triggered "captureblocked" (cp "1") (cpname "Control Point B") (position "-2143 2284 156")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "captureblocked" \x28cp "(\d+)"\x29 \x28cpname "#?(.*)"\x29 \x28position "(.*)"\x29', logdata)
+        if (res):
+            print "Capture blocked"
+            pprint(res.groups())
+
+            return
+
+        #domination
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "domination" against "(.*)<(\d+)><(.*)><(Red|Blue)>"', logdata)
+        if (res):
+            print "Player dominated"
+            pprint(res.groups())
+
+            return
+
+        #revenge
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "revenge" against "(.*)<(\d+)><(.*)><(Red|Blue)>"', logdata)
+        if (res):
+            print "Player got revenge"
+            pprint(res.groups())
+
+            return
+        
+        #suicide
+        #"Hypnos<20><STEAM_0:0:24915059><Red>" committed suicide with "world" (customkill "train") (attacker_position "568 397 -511")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" committed suicide with "(.*)" \x28customkill "(.*?)"\x29', logdata)
+        if (res):
+            print "Player committed suicide"
+            pprint(res.groups())
+
+            return
+
+        #current score (shown after round win/round length
+        res = regex(r'Team "(Blue|Red)" current score "(\d+)" with "(\d+)" players', logdata)
+        if (res):
+            print "Current scores"
+            pprint(res.groups())
+
+            return
+
+        #engi building destruction
+        #"dcup<109><STEAM_0:0:15236776><Red>" triggered "killedobject" (object "OBJ_SENTRYGUN") (weapon "tf_projectile_pipe") (objectowner "NsS. oLiVz<101><STEAM_0:1:15674014><Blue>") (attacker_position "551 2559 216")
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" triggered "killedobject" \x28object "(.*)"\x29 \x28weapon "(.*)"\x29 \x28objectowner "(.*)<(\d+)><(.*)><(Blue|Red)>)"\x29 \x28attacker_position "(.*)"\x29', logdata)
+        if (res):
+            print "Player destroyed engineer building"
+            pprint(res.groups())
+
+            return
+
+        #final scores
+        res = regex(r'Team "(Blue|Red)" final score "(\d+)" with "(\d+)" players', logdata)
+        if (res):
+            print "Final scores"
+            pprint(res.groups())
+
+            return
+
+        #game over
+        res = regex(r'World triggered "Game_Over" reason "(.*)"', logdata)
+        if (res):
+            print "Game over"
+            pprint(res.groups())
 
             return
 
         #rcon command
-        res = regex('rcon from "(.*?)": command "(.*)"', logdata)
+        res = regex(r'rcon from "(.*?)": command "(.*)"', logdata)
         if (res):
             print "Someone issued rcon command"
             pprint(res.groups())
 
             return
+
+
         #disconnect RL 10/07/2012 - 01:13:44: "triple h<162><STEAM_0:1:33713004><Red>" disconnected (reason " #tf2pug")
-        res = regex('"(.*)<(\d+)><(.*)><(Red|Blue)>" disconnected \(reason "(.*)"\)', logdata)
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" disconnected \(reason "(.*)"\)', logdata)
         if (res):
             print "Player disconnected"
             pprint(res.groups())
-
+            
             return
-
+        
         #connect RL 10/07/2012 - 22:45:11: "GU | wm<3><STEAM_0:1:7175436><>" connected, address "124.168.51.7:27005"
-        res = regex('"(.*)<(\d+)><(.*)><>" connected, address "(.*?):(.*)"', logdata)
+        res = regex(r'"(.*)<(\d+)><(.*)><>" connected, address "(.*?):(.*)"', logdata)
         if (res):
             print "Player connected"
             pprint(res.groups())
 
             return
         #validated "hipsterhipster<4><STEAM_0:1:22674758><>" STEAM USERID validated
+        res = regex(r'"(.*)<(\d+)><(.*)><>" STEAM USERID validated', logdata)
+        if (res):
+            print "Player validated"
+            pprint(res.groups())
 
+            return
+        #chat
+        res = regex(r'"(.+)<(\d+)><(.+)><(Red|Blue|Spectator)>" (say|say_team) "(.+)"', logdata)
+        if (res):
+            print "Chat was said"
+            pprint(res.groups())
+
+            return
+        
+        #class change    
+        res = regex(r'"(.*)<(\d+)><(.*)><(Red|Blue)>" changed role to "(.*)"', logdata)
+        if (res):
+            print "Player changed class"
+            pprint(res.groups())
+
+            return
+
+        #round win
+        res = regex(r'World triggered "Round_Win".+winner.+"(Blue|Red)"', logdata)
+        if (res):
+            print "Round won"
+            pprint(res.groups())
+
+            return
+
+        #overtime
+        res = regex(r'World triggered "Round_Overtime"', logdata)
+        if (res):
+            print "Overtime"
+            pprint(res.groups())
+
+            return
+
+        #round length
+        res = regex(r'World triggered "Round_Length".+seconds.+"(\d+.\d+)', logdata)
+        if (res):
+            print "Round length"
+            pprint(res.groups())
+    
+            return
+
+        #round start
+        res = regex(r'World triggered "Round_Start"', logdata)
+        if (res):
+            print "Round start"
+            pprint(res.groups())
+
+            return
+
+        #setup end
+        res = regex(r'World triggered "Round_Setup_End"', logdata)
+        if (res);
+            print "Round Setup End"
+            pprint(res.groups())
+
+            return
+
+        #mini round win
+        res = regex(r'World triggered "Mini_Round_Win" \x28winner "(Blue|Red)"\x29 \x28round "round_(\d+)"\x29', logdata)
+        if (res):
+            print "Mini round win"
+            pprint(res.groups())
+
+            return
+
+        #mini round length
+        res = regex(r'World triggered "Mini_Round_Length" \x28seconds "(\d+.\d+)"\x29', logdata)
+        if (res):
+            print "Mini round length"
+            pprint(res.groups())
+
+            return
 
     def regex(self, expression, string): #helper function for performing regular expression checks. avoids having to compile and match in-function 1000 times
         preg = re.compile(expression, re.IGNORECASE | re.MULTILINE)
-
+        
         match = preg.search(string)
 
         print match
         return match
+
+
---
+++ sql/table_functions.sql
@@ -0,0 +1,84 @@
+﻿CREATE OR REPLACE FUNCTION create_game_event_table (unique_id text) RETURNS void AS $_$
+DECLARE
+	table_name varchar(128);
+BEGIN
+	table_name := 'log_event_' || unique_id;
+	
+	EXECUTE 'CREATE TABLE ' || table_name || ' (eventid serial PRIMARY KEY, time text, event_type text)';
+END;
+$_$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION create_game_stat_table (unique_id text) RETURNS void AS $_$
+DECLARE
+	table_name varchar(128);
+BEGIN
+	table_name := 'log_stat_' || unique_id;
+	
+	EXECUTE 'CREATE TABLE ' || table_name || ' (steamid varchar(64) PRIMARY KEY, name text, kills integer, deaths integer, assists integer, points integer, healing_done integer,
+					     healing_received integer, ubers_used integer, ubers_lost integer, damage_dealt integer, ap_small integer, ap_medium integer, ap_large integer,
+					     mk_small integer, mk_medium integer, mk_large integer, captures integer, captures_blocked integer, dominations integer, revenges integer,
+					     suicides integer, buildings_destroyed integer)';
+END;
+$_$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION create_game_kill_table (unique_id text) RETURNS void AS $_$
+DECLARE
+	table_name varchar(128);
+BEGIN
+	table_name := 'log_kill_' || unique_id;
+	
+	EXECUTE 'CREATE TABLE ' || table_name || ' (eventid integer PRIMARY KEY, attacker_id varchar(64), attacker_pos varchar(32),
+	                                                                         assister_id varchar(64), assister_pos varchar(32),
+	                                                                         victim_id varchar(64), victim_pos varchar(32))';
+END;
+$_$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION create_game_chat_table (unique_id text) RETURNS void AS $_$
+DECLARE
+	table_name varchar(128);
+BEGIN
+	table_name := 'log_chat_' || unique_id;
+	
+	EXECUTE 'CREATE TABLE ' || table_name || ' (eventid integer PRIMARY KEY, steamid varchar(64), chat_type varchar(12), chat_message text)';
+END;
+$_$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION create_game_round_table (unique_id text) RETURNS void AS $_$
+DECLARE
+	table_name varchar(128);
+BEGIN
+	table_name := 'log_round_' || unique_id;
+	
+	EXECUTE 'CREATE TABLE ' || table_name || ' (eventid integer PRIMARY KEY, red_score integer, blue_score integer, round_length decimal)';
+END;
+$_$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION setup_log_tables (unique_id text) RETURNS void AS $_$
+BEGIN
+	PERFORM create_game_event_table(unique_id);
+	PERFORM create_game_stat_table(unique_id);
+	PERFORM create_game_kill_table(unique_id);
+	PERFORM create_game_chat_table(unique_id);
+	PERFORM create_game_round_table(unique_id);
+END;
+$_$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION create_global_stat_table () RETURNS void AS $_$
+BEGIN
+	IF EXISTS (
+		SELECT *
+		FROM pg_catalog.pg_tables
+		WHERE schemaname = 'livelogs'
+		AND tablename = 'livelogs_player_stats'
+		)
+	THEN
+		RAISE NOTICE 'Table livelogs.livelogs_player_stats already exists';
+	ELSE
+		CREATE TABLE livelogs.livelogs_player_stats ();
+	END IF;
+END;
+$_$ LANGUAGE 'plpgsql';
