966f2273ee0cd3f511e7e72c098fab642c7d63ad
Prithu Parker <prithu@iinet.net.au>
2012-11-15 00:08:15 +1100

    servers table now has a numerical index for sorting, fixed jquery tables css to properly have images

--- sql/table_functions.sql
+++ sql/table_functions.sql
@@ -98,7 +98,7 @@ BEGIN
 	THEN
 		RAISE NOTICE 'Table livelogs.livelogs_servers already exists';
 	ELSE
-		CREATE TABLE livelogs_servers (server_ip varchar(32) NOT NULL, server_port integer NOT NULL, log_ident varchar(64) PRIMARY KEY, map varchar(64) NOT NULL, log_name text, live boolean);
+		CREATE TABLE livelogs_servers (numeric_id serial, server_ip varchar(32) NOT NULL, server_port integer NOT NULL, log_ident varchar(64),  map varchar(64) NOT NULL, log_name text, live boolean, PRIMARY KEY (numeric_id, log_ident));
 	END IF;
 END;
 $_$ LANGUAGE 'plpgsql';
