16122682f57c6812c3c20eb01c1ffd6db2b98c31
Prithu Parker <prithu@iinet.net.au>
2013-03-11 00:08:49 +1100

    merge?

--- test/testclient.py
+++ test/testclient.py
@@ -1,49 +1,63 @@
 import socket
 import time
+
+def connect():
+    print "Creating client socket"
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        #client.connect(('119.15.97.230', 61222))
+        client.connect(('192.168.35.128', 61222))
+    except:
+        quit()
  
-print "Creating client socket"
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #testm = "LIVELOG!ADVENTUREBEWITHYOU!202.161.23.120!27015!cp_granary!JIMBOBJUNIOR3"
+    testm = "LIVELOG!new_api_key!192.168.35.1!27015!cp_granary!newer name here!23142"
+    slen = client.send(testm)
 
-try:
-    client.connect(('119.15.97.230', 61222))
-    #client.connect(('192.168.35.128', 61222))
-except:
-    quit()
+    rsp = client.recv(1024)
  
-testm = "LIVELOG!ADVENTUREBEWITHYOU!202.161.23.120!27015!cp_granary!JIMBOBJUNIOR3"
-#testm = "LIVELOG!!192.168.35.1!27015!cp_granary!newer name here!23142"
-slen = client.send(testm)
+    print "Server responded: %s" % rsp
+    process_response(rsp)
 
-rsp = client.recv(1024)
+def process_response(rsp):
+    #LIVELOG!123test!192.168.35.128!42276!UNIQUE_IDENT OR REUSE
+    tokenized = rsp.split('!')
+    #LIVELOG!123test!192.168.35.128!57221!3232244481_27015_1358162178
+    if (len(tokenized) <= 5):
+        if (tokenized[4] != "REUSE"):
+            client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            #client.connect(("192.168.35.128", 57221))
+            client.connect((tokenized[2], int(tokenized[3])))
 
-#client.close()
- 
-print "Server responded: %s" % rsp
+            #client.send('L 10/01/2012 - 21:38:34: "Liquid\'zato<46><STEAM_0:0:42607036><Blue>" killed "[v3] Faithless<47><STEAM_0:0:52150090><Red>" with "tf_projectile_rocket" (attacker_position "-1158 -194 295") (victim_position "-1200 197 308")')
 
-#LIVELOG!123test!192.168.35.128!42276!UNIQUE_IDENT OR REUSE
-tokenized = rsp.split('!')
-#LIVELOG!123test!192.168.35.128!57221!3232244481_27015_1358162178
-if (len(tokenized) <= 5):
-    if (tokenized[4] != "REUSE"):
-        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #client.connect(("192.168.35.128", 57221))
-        client.connect((tokenized[2], int(tokenized[3])))
+            log_file = open(r'E:\Git\livelogs\test\test_log.log')
 
-        #client.send('L 10/01/2012 - 21:38:34: "Liquid\'zato<46><STEAM_0:0:42607036><Blue>" killed "[v3] Faithless<47><STEAM_0:0:52150090><Red>" with "tf_projectile_rocket" (attacker_position "-1158 -194 295") (victim_position "-1200 197 308")')
+            x = 0
 
-        log_file = open(r'E:\Git\livelogs\test\test_log.log')
-        
-        for logline in log_file:
-            print logline
-            client.send(logline)
+            for logline in log_file:
+                print logline
+                client.send(logline)
 
-            time.sleep(0.05)
+                """if x is 100:
+                    client.send('L 10/01/2012 - 21:38:34: "LIVELOG_GAME_RESTART"')
 
-        log_file.close()
+                    break
+                """
+                x += 1
+                time.sleep(0.01)
 
-        client.close()
-        
+            log_file.close()
+
+            client.close()
+
+            #connect()
+            
+            
+        else:
+            print "livelogs daemon told us to re-use... don't want to do that with test client!"
     else:
-        print "livelogs daemon told us to re-use... don't want to do that with test client!"
-else:
-    print "Invalid response"
+        print "Invalid response"
+
+connect()
