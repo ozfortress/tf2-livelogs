a258723f8cc19d306c66bfd394461c215b83693d
Prithu Parker <prithu@iinet.net.au>
2013-07-16 00:27:17 +1000

    paging works with first & last controls, pipelining also working

--- web/dev/js/playerview.js
+++ web/dev/js/playerview.js
@@ -1,6 +1,10 @@
-/* Bootstrap style pagination. Author: Allan Jardine @ http://sprymedia.co.uk/
+/* 
+    Bootstrap style pagination. Author: Allan Jardine @ http://sprymedia.co.uk/
 
-/* API method to get paging information */
+    Modifed to have first & last controls
+ */
+
+//API method to get paging information 
 $.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
 {
     return {
@@ -30,17 +34,21 @@ $.extend( $.fn.dataTableExt.oPagination, {
  
             $(nPaging).addClass('pagination').append(
                 '<ul>'+
-                    '<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
-                    '<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
+                    '<li class="first disabled"><a href="#">&larr; '+oLang.sFirst+'</a></li>'+
+                    '<li class="prev disabled"><a href="#">'+oLang.sPrevious+'</a></li>'+
+                    '<li class="next disabled"><a href="#">'+oLang.sNext+'</a></li>'+
+                    '<li class="last disabled"><a href="#">'+oLang.sLast+' &rarr;</a></li>'+
                 '</ul>'
             );
             var els = $('a', nPaging);
-            $(els[0]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
-            $(els[1]).bind( 'click.DT', { action: "next" }, fnClickHandler );
+            $(els[0]).bind( 'click.DT', { action: "first" }, fnClickHandler );
+            $(els[1]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
+            $(els[2]).bind( 'click.DT', { action: "next" }, fnClickHandler );
+            $(els[3]).bind( 'click.DT', { action: "last" }, fnClickHandler );
         },
  
         "fnUpdate": function ( oSettings, fnDraw ) {
-            var iListLength = 5;
+            var iListLength = 8;
             var oPaging = oSettings.oInstance.fnPagingInfo();
             var an = oSettings.aanFeatures.p;
             var i, j, sClass, iStart, iEnd, iHalf=Math.floor(iListLength/2);
@@ -60,15 +68,17 @@ $.extend( $.fn.dataTableExt.oPagination, {
                 iEnd = iStart + iListLength - 1;
             }
  
+            var paging_ul = null;
             for ( i=0, iLen=an.length ; i<iLen ; i++ ) {
                 // Remove the middle elements
-                $('li:gt(0)', an[i]).filter(':not(:last)').remove();
+                paging_ul = $(an[i]).children().children();
+                $(paging_ul).slice(2, paging_ul.length-2).remove(); //remove elements between the controls
  
                 // Add the new list items and their event handlers
                 for ( j=iStart ; j<=iEnd ; j++ ) {
                     sClass = (j==oPaging.iPage+1) ? 'class="active"' : '';
                     $('<li '+sClass+'><a href="#">'+j+'</a></li>')
-                        .insertBefore( $('li:last', an[i])[0] )
+                        .insertBefore( $('li:nth-last-child(2)', an[i])[0] ) //insert before the 2nd last element
                         .bind('click', function (e) {
                             e.preventDefault();
                             oSettings._iDisplayStart = (parseInt($('a', this).text(),10)-1) * oPaging.iLength;
@@ -79,14 +89,18 @@ $.extend( $.fn.dataTableExt.oPagination, {
                 // Add / remove disabled classes from the static elements
                 if ( oPaging.iPage === 0 ) {
                     $('li:first', an[i]).addClass('disabled');
+                    $('li:nth-child(2)', an[i]).addClass('disabled');
                 } else {
                     $('li:first', an[i]).removeClass('disabled');
+                    $('li:nth-child(2)', an[i]).removeClass('disabled');
                 }
  
                 if ( oPaging.iPage === oPaging.iTotalPages-1 || oPaging.iTotalPages === 0 ) {
                     $('li:last', an[i]).addClass('disabled');
+                    $('li:nth-last-child(2)', an[i]).addClass('disabled');
                 } else {
                     $('li:last', an[i]).removeClass('disabled');
+                    $('li:nth-last-child(2)', an[i]).removeClass('disabled');
                 }
             }
         }
@@ -115,6 +129,12 @@ $(document).ready(function() {
 });
 
 var ll_paging = ll_paging || (function() {
+    var pipe_cache = {
+            cache_start: -1,
+            cache_end: -1,
+            last_request: null
+        };
+
     return {
         init : function(community_id, num_preloaded, total_logs) {
             console.log("cid: %s | num_preloaded: %s | total: %s", community_id, num_preloaded, total_logs);
@@ -142,20 +162,107 @@ var ll_paging = ll_paging || (function() {
                 "bServerSide": true,
                 "sAjaxSource": "/func/paging_data.php",
                 "iDeferLoading": total_logs,
-                //"fnServerData": ll_paging.datatables_pipeline,
+                "fnServerData": ll_paging.datatables_pipeline,
                 "fnServerParams": function (aoData) {
                     aoData.push({ "name": "cid", "value": community_id });
                 }
             });
         },
 
-        datatables_pipeline : function (source, data, callback)
-        {
-            var pipe_len = 4; /* how many pages to preload */
+        datatables_getkey : function(data, key) {
+            for (var i = 0, dlen = data.length; i < dlen; i++) {
+                if (data[i].name === key) {
+                    return data[i].value;
+                }
+            }
 
-
+            return null;
         },
 
+        datatables_setkey : function(data, key, value) {
+            for (var i = 0, dlen = data.length; i < dlen; i++) {
+                if (data[i].name === key) {
+                    data[i].value = value;
+                }
+            }
+        },
+
+        datatables_pipeline : function(data_source, request_data, datatables_callback) {
+            var pipe_len = 8, /* how many pages to preload */
+            need_server = false, 
+            challenge_echo = ll_paging.datatables_getkey(request_data, "sEcho"),
+            request_start = ll_paging.datatables_getkey(request_data, "iDisplayStart"),
+            display_length = ll_paging.datatables_getkey(request_data, "iDisplayLength"),
+            request_end = request_start + display_length;
+
+            pipe_cache.display_start = request_start;
+
+            /* check if the requested data is outside the cache or not */
+            if (pipe_cache.cache_start < 0 || request_start < pipe_cache.cache_start || request_end > pipe_cache.cache_end) {
+                //data is outside the cache, so we need to get more data from the server
+                need_server = true;
+            }
+
+            /* check if the sorting has changed */
+            if (pipe_cache.last_request && !need_server) {
+                for (var i = 0, dlen = request_data.length; i < dlen; i++) {
+                    if (request_data[i].name !== "iDisplayStart" && request_data[i].name !== "iDisplayLength" && request_data[i].name !== "sEcho") {
+                        if (request_data[i].value != pipe_cache.last_request[i].value) {
+                            need_server = true //data is different from what is cached, we need to re-cache
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //store the request for the next check
+            pipe_cache.last_request = request_data.slice();
+
+            if (need_server) {
+                /* let's get some data from daaa server */
+
+                //if the requested starting position is below the the cache's lowest position, we want to go down a page
+                if (request_start < pipe_cache.cache_start) {
+                    request_start = request_start - display_length * (pipe_len - 1);
+
+                    if (request_start < 0) {
+                        request_start = 0;
+                    }
+                }
+
+                pipe_cache.cache_start = request_start;
+                pipe_cache.cache_end = request_start + (display_length * pipe_len);
+                pipe_cache.display_length = display_length;
+
+                //set the request_data's lengths to the lengths of the pipeline
+                ll_paging.datatables_setkey(request_data, "iDisplayStart", request_start);
+                ll_paging.datatables_setkey(request_data, "iDisplayLength", display_length * pipe_len);
+
+                $.getJSON(data_source, request_data, function(json) {
+                    pipe_cache.last_json = jQuery.extend(true, {}, json);
+
+                    if (pipe_cache.cache_start != pipe_cache.display_start) {
+                        //if the cache start != the display start, we have to splice from 0 up to the cache starting position first
+                        json.aaData.splice(0, pipe_cache.display_start - pipe_cache.cache_start);
+                    }
+
+                    json.aaData.splice(pipe_cache.display_length, json.aaData.length); //split the return data in a segment of display_length for returning
+
+                    datatables_callback(json);
+                });    
+            }
+            else {
+                //dont need to request data from the server
+                json = jQuery.extend(true, {}, pipe_cache.last_json); //get our json out from the last server request
+
+                json.sEcho = challenge_echo;
+                json.aaData.splice(0, request_start - pipe_cache.cache_start);
+                json.aaData.splice(display_length, json.aaData.length);
+
+                datatables_callback(json);
+            }
+
+        },
 
     }
 }());
