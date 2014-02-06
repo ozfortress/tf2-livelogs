8fe066db3fc49b31565cdd6975406f1618cfafe6
Prithu Parker <prithu@iinet.net.au>
2014-02-06 17:59:47 +1100

    implementing simple api for user stats for tf2pug

--- web/dev/api/main.php
+++ web/dev/api/main.php
@@ -1,28 +1,90 @@
 <?php
-	require "../../conf/ll_database.php";
+    require "../../conf/ll_database.php";
 
-	if (isset($_GET['action'])) 
-	{
-		$output = array();
+    if (!isset($_GET["key"]))
+    {
+        header("HTTP/1.0 403 Forbidden");
+        pg_close($ll_db);
+        exit;
+    }
 
-		if ($_GET['action'] === "get_live")
-		{
-			$live_query = "SELECT log_ident FROM livelogs_log_index WHERE live='true'";
-			$live_res = pg_query($ll_db, $live_query);
+    $escaped_key = pg_escape_string($_GET["key"])
+    $key_query = "SELECT EXISTS (SELECT 1 FROM livelogs_auth_keys WHERE user_key = '{$escaped_key}')";
+    $key_res = pg_query($ll_db, $key_query);
 
-			if ($live_res)
-			{
-				$output["live"] = array();
+    if (pg_num_rows($key_res) == 0)
+    {
+        header("HTTP/1.1 401 Unauthorized");
+        pg_close($ll_db);
+        exit;
+    }
+    
 
-				while ($row = pg_fetch_array($live_res, NULL, PGSQL_ASSOC))
-				{
-					$output["live"][] = $row["log_ident"];
-				}
-			}
+    if (isset($_GET["action"])) 
+    {
+        $action = $_GET["action"];
+        $output = array();
 
-			echo json_encode($output);
-		}
-	}
+        if ($action === "get_live")
+        {
+            $live_query = "SELECT log_ident FROM livelogs_log_index WHERE live='true'";
+            $live_res = pg_query($ll_db, $live_query);
 
-	pg_close($ll_db);
+            if ($live_res)
+            {
+                $output["idents"] = array();
+
+                while ($row = pg_fetch_array($live_res, NULL, PGSQL_ASSOC))
+                {
+                    $output["idents"][] = $row["log_ident"];
+                }
+            }
+        }
+        else if ($action === "get_stats")
+        {
+            $steamids = isset($_GET["steamids"]) ? $_GET["steamids"] : "";
+
+            $sidarray = explode(",", $steamids);
+            $escaped_ids = array_map("pg_escape_string", $sidarray);
+
+            $filter = "WHERE steamid IN {$escaped_ids}";
+
+            // support the selection of only stats from this API key
+            if (isset($_GET["key_only"]))
+            {
+                $filter .= " AND log_ident IN (SELECT log_ident FROM livelogs_log_index WHERE api_key = '{$escaped_key}'')";
+            }
+
+            $query = "SELECT steamid,
+                         SUM(kills) as kills, SUM(deaths) as deaths, SUM(assists) as assists,
+                         SUM(captures) as captures, SUM(headshots) as headshots,
+                         SUM(healing_done) as healing_done, SUM(overhealing_done) as overhealing_done,
+                         SUM(damage_dealt) as damage_dealt,
+                         COUNT(log_ident) as numplayed
+                      FROM livelogs_player_stats
+                      {$filter}
+                      GROUP BY steamid";
+
+            $result = pg_query($ll_db, $query);
+
+            if ($result)
+            {
+                $output["stats"] = array();
+                while ($row = pg_fetch_array($result, NULL, PGSQL_ASSOC))
+                {
+                    $output["stats"][$row["steamid"]] = $row;
+                }
+
+            }
+        }
+
+        echo json_encode($output);
+    }
+    else
+    {
+        header("HTTP/1.0 400 Bad Request");
+    }
+
+    pg_close($ll_db);
+
 ?>
